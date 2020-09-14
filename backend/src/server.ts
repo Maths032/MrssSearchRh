37cb433db6ca50ed54346940a7f45e57e051f98d
Matheus <matheus.ramos.silva.santos@gmail.com>
2020-09-13 22:56:05 -0300

    eslint, prettier and editorconfig configured

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello world'})
-})
-
+app.get('/', (request, response) => response.json({ message: 'Hello world' }));
 
 app.listen(3333, () => {
-    console.log('server started!');
-})
+  console.log('server started!');
+});
